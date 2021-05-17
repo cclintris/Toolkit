83d05de7867a2e29d07cb0ad342504e690fe4446
cclin <181250083@smail.nju.edu.cn>
2021-05-17 21:31:12 +0800

    complete: ChineseTrans module function(need reconstruct)

--- src/components/views/ChineseTrans.jsx
+++ src/components/views/ChineseTrans.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import "../style/ChineseTrans.css";
-import { Input, Button } from "antd";
+import { Input, Button, message } from "antd";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { traditionalized, simplized } from "../../utils/chineseTrans";
 
 const { TextArea } = Input;
 
 const ChineseTrans = () => {
   const [input, setInput] = useState("");
+  const inputRef = useRef();
+  const checkTextExist = useMemo(() => {
+    return input === null || input === undefined || input === "";
+  }, [input]);
 
   const onChange = (e) => {
     setInput(e.target.value);
+  };
+
+  const copyToClipboard = () => {
+    if (checkTextExist) {
+      message.error("先輸入文字！");
+      inputRef.current.focus();
+    } else {
+      try {
+        message.success("複製成功！");
+      } catch (e) {
+        message.error("錯誤發生！");
+      }
+    }
   };
 
   return (
@@ -19,13 +37,19 @@ const ChineseTrans = () => {
         onChange={onChange}
         placeholder="輸入文字"
         style={{ marginBottom: "30px", overflow: "scroll", height: "55vh" }}
+        ref={inputRef}
       />
       <div className="CT-btn">
         <Button
           type="primary"
           style={{ marginRight: "20px" }}
           onClick={() => {
-            setInput(traditionalized(input));
+            if (!checkTextExist) {
+              setInput(traditionalized(input));
+              message.info("簡轉繁");
+            } else {
+              message.error("先輸入文字！");
+            }
           }}
         >
           簡轉繁
@@ -34,12 +58,38 @@ const ChineseTrans = () => {
           type="primary"
           style={{ marginRight: "20px" }}
           onClick={() => {
-            setInput(simplized(input));
+            if (!checkTextExist) {
+              setInput(simplized(input));
+              message.info("繁轉簡");
+            } else {
+              message.error("先輸入文字！");
+            }
           }}
         >
           繁轉簡
         </Button>
-        <Button type="dashed">複製的剪貼板</Button>
+        <CopyToClipboard text={input}>
+          <Button
+            type="dashed"
+            style={{ marginRight: "20px" }}
+            onClick={copyToClipboard}
+          >
+            複製到剪貼板
+          </Button>
+        </CopyToClipboard>
+        <Button
+          danger
+          type="dashed"
+          onClick={() => {
+            if (!checkTextExist) {
+              setInput("");
+            } else {
+              message.error("先輸入文字！");
+            }
+          }}
+        >
+          清空文字
+        </Button>
       </div>
     </div>
   );
